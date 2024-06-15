5b8377d4bd1bdd402d713da464cda1b97461bbcf
Fateme <Fateme.bd7@gmail.com>
2024-06-15 12:09:42 -0700

    Added Is Valid move (is safe) Function

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -4,3 +4,19 @@ def find_empty_location(grid):
             if grid[row][col] == 0:
                 return row, col
     return None
+
+
+def is_safe(grid, row, col, num):
+    if num in grid[row]:
+        return False
+
+    if num in [grid[i][col] for i in range(9)]:
+        return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if grid[start_row + i][start_col + j] == num:
+                return False
+
+    return True
