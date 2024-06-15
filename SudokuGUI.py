2aac632ce5c6c30865c1f296039733d5fdd76b50
Fateme <Fateme.bd7@gmail.com>
2024-06-15 11:34:36 -0700

    Added grid SudokuGui.py

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -6,8 +6,14 @@ class SudokuGUI:
         self.cells = [[tk.Entry(root, width=3,bg="#ffcc99", font=('Arial', 18),
         justify='center', bd=3, relief='raised')
         for _ in range(9)] for _ in range(9)]
-        
+        self.create_grid()
 
+     def create_grid(self):
+        for i in range(9):
+            for j in range(9):
+                self.cells[i][j].grid(row=i, column=j, padx=3, pady=3)
+                self.cells[i][j].config(highlightthickness=1.5, highlightbackground='gray')
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
